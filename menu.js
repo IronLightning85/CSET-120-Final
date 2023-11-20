56f63b8cec4547c5060b48a24f3eac9982c35af0
EthanFriedrichs <ethanpfriedrichs3@gmail.com>
2023-11-20 08:03:53 -0500

    Added JSON values

---
+++ menu.js
@@ -0,0 +1,10 @@
+let menu = '{ "Menu" : [' +
+    '{"name":"Veggie Cream Soup", "price":"TBD", "url":"images/food/Creamy Soup.JPG"},' +
+    '{"name":"Nut Cake", "price":"TBD", "url":"images/food/Nut Cake.jpeg"}, ' +
+    '{"name":"Monster Rice Balls", "price":"TBD", "url":"images/food/BotW Monster Rice Ball.jpeg"}, ' +
+    '{"name":"Mud Cookie", "price":"TBD", "url":"images/food/mudcookie_08_copy.jpg"}, ' +
+    '{"name":"Cheesy Tomato", "price":"TBD", "url":"images/food/cheesyTomato_01.png"}, ' +
+    '{"name":"Monster Lasagna", "price":"TBD", "url":"images/food/monsterLasagna_01.jpg"}, ' +
+    '{"name":"Firecap Soup", "price":"TBD", "url":"images/food/01_FirecapSoup.jpg"}, ' +
+    '{"name":"Mango Rice", "price":"TBD", "url":"images/food/mangoRice_01.jpg"}, ]}';
+
